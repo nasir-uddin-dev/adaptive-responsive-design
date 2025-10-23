fcf61bdbba33949956548c94d6484b8be0b9e058
nasir-uddin-dev <nasiruddin3954@gmail.com>
2025-10-23 18:03:08 +0600

    focus-traversal-group-example

---
+++ lib/focus_traversal_group_example_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FocusTraversalGroupExample extends StatefulWidget {
+  const FocusTraversalGroupExample({super.key});
+
+  @override
+  State<FocusTraversalGroupExample> createState() => _FocusTraversalGroupExampleState();
+}
+
+class _FocusTraversalGroupExampleState extends State<FocusTraversalGroupExample> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,18 @@
-import 'package:adaptive_responsive_design/List_view/focus_able_action_detector_widget.dart';
 import 'package:flutter/material.dart';
+import 'focus_traversal_group_example_app.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const FocusTraversalGroupExampleApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class FocusTraversalGroupExampleApp extends StatelessWidget {
+  const FocusTraversalGroupExampleApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: FocusAbleActionDetectorExample(),
+      home: FocusTraversalGroupExample(),
     );
   }
 }
