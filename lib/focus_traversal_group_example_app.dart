4345d6120634260d4ef67b9c028a294ef26d48b9
nasir-uddin-dev <nasiruddin3954@gmail.com>
2025-10-23 18:51:59 +0600

    focus-traversal-group-example

--- lib/focus_traversal_group_example_app.dart
+++ lib/focus_traversal_group_example_app.dart
@@ -1,15 +1,68 @@
 import 'package:flutter/material.dart';
 
+/// A button wrapper that adds either a numerical or lexical order, depending on
+/// the type of T
+class OrderedButton<T> extends StatefulWidget {
+  const OrderedButton({
+    super.key,
+    required this.name,
+    required this.canRequestFocus,
+    required this.autofocus,
+    required this.order,
+  });
+
+  final String name;
+  final bool canRequestFocus;
+  final bool autofocus;
+  final T order;
+
+  @override
+  State<OrderedButton> createState() => _OrderedButtonState();
+}
+
+class _OrderedButtonState extends State<OrderedButton> {
+  late FocusNode focusNode;
+  @override
+  Widget build(BuildContext context) {
+    final FocusOrder order = switch (widget.order) {
+      final num number => NumericFocusOrder(number.toDouble()),
+      final Object? object => LexicalFocusOrder(object.toString()),
+    };
+    return FocusTraversalOrder(
+      order: order,
+      child: Padding(padding: const EdgeInsets.all(8),
+        child: OutlinedButton(
+          focusNode: focusNode,
+            autofocus: widget.autofocus,
+            style: ButtonStyle(
+              overlayColor:
+            ),
+            onPressed: onPressed, child: child),
+      ),
+    );
+  }
+}
+
 class FocusTraversalGroupExample extends StatefulWidget {
   const FocusTraversalGroupExample({super.key});
 
   @override
-  State<FocusTraversalGroupExample> createState() => _FocusTraversalGroupExampleState();
+  State<FocusTraversalGroupExample> createState() =>
+      _FocusTraversalGroupExampleState();
 }
 
-class _FocusTraversalGroupExampleState extends State<FocusTraversalGroupExample> {
+class _FocusTraversalGroupExampleState
+    extends State<FocusTraversalGroupExample> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return SafeArea(
+      child: ColoredBox(
+        color: Colors.grey,
+        child: FocusTraversalGroup(
+          policy: OrderedTraversalPolicy(),
+          child: Column(children: []),
+        ),
+      ),
+    );
   }
 }
