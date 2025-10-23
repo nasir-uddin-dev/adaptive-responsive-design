52ab2eddc72cedbfb2c65003f0bdf3dd5c6b43f0
nasir-uddin-dev <nasiruddin3954@gmail.com>
2025-10-23 16:11:43 +0600

    focusAbleActionDetector

--- lib/List_view/focus_able_action_detector_widget.dart
+++ lib/List_view/focus_able_action_detector_widget.dart
@@ -3,17 +3,126 @@
 // and provides callbacks for handling focus and hover highlights.
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class FadButton extends StatefulWidget {
-  const FadButton({super.key});
+  const FadButton({super.key, required this.onPressed, required this.child});
+
+  final VoidCallback onPressed;
+  final Widget child;
 
   @override
   State<FadButton> createState() => _FadButtonState();
 }
 
 class _FadButtonState extends State<FadButton> {
+  bool _focused = false;
+  bool _hovering = false;
+  bool _on = false;
+
+  late final Map<Type, Action<Intent>> _actionMap;
+  final Map<ShortcutActivator, Intent> _shortcutMap = const {
+    SingleActivator(LogicalKeyboardKey.keyX): ActivateIntent(),
+  };
+
+  @override
+  void initState() {
+    super.initState();
+    _actionMap = {
+      ActivateIntent: CallbackAction(
+        onInvoke: (Intent intent) => _toggleState(),
+      ),
+    };
+  }
+
+  Color get color {
+    Color baseColor = Colors.lightBlue;
+    if (_focused) {
+      baseColor = Color.alphaBlend(Colors.black.withOpacity(0.25), baseColor);
+    }
+    if (_hovering) {
+      baseColor = Color.alphaBlend(Colors.black.withOpacity(0.1), baseColor);
+    }
+    return baseColor;
+  }
+
+  void _toggleState() {
+    setState(() {
+      _on = !_on;
+    });
+  }
+
+  void _handleFocusHighlight(bool value) {
+    setState(() {
+      _focused = value;
+    });
+  }
+
+  void _handleHoverHighlight(bool value) {
+    setState(() {
+      _hovering = value;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return GestureDetector(
+      onTap: _toggleState,
+      child: FocusableActionDetector(
+        actions: _actionMap,
+        shortcuts: _shortcutMap,
+        onShowFocusHighlight: _handleFocusHighlight,
+        onShowHoverHighlight: _handleHoverHighlight,
+        child: Row(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(10),
+              color: color,
+              child: widget.child,
+            ),
+            Container(
+              width: 30,
+              height: 30,
+              margin: const EdgeInsets.all(10),
+              color: _on ? Colors.red : Colors.transparent,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class FocusAbleActionDetectorExample extends StatefulWidget {
+  const FocusAbleActionDetectorExample({super.key});
+
+  @override
+  State<FocusAbleActionDetectorExample> createState() =>
+      _FocusAbleActionDetectorExampleState();
+}
+
+class _FocusAbleActionDetectorExampleState
+    extends State<FocusAbleActionDetectorExample> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("FocusAbleActionDetector Example")),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: TextButton(onPressed: () {}, child: Text('Press Me')),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: FadButton(onPressed: () {}, child: Text('Add Me')),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
